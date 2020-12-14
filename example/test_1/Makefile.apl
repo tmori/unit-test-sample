be8d31afa72d0b31f3e01d8141ea80406fe536fa
tmori@esm.co.jp <tmori@esm.co.jp>
2020-12-14 11:41:00 +0900

    add platform and sample

---
+++ example/test_1/Makefile.apl
@@ -0,0 +1,5 @@
+APPL_CFG += sample1.cfg
+
+APPLDIR	+= $(APL_DIR)
+
+APPL_COBJS	+= sample1.o
---
+++ example/test_1/sample1.c
@@ -0,0 +1,36 @@
+#include <kernel.h>
+#include <t_syslog.h>
+#include <t_stdlib.h>
+#include "syssvc/serial.h"
+#include "syssvc/syslog.h"
+#include "kernel_cfg.h"
+#include "sample1.h"
+
+
+/*
+ *  サービスコールのエラーのログ出力
+ */
+Inline void
+svc_perror(const char *file, int_t line, const char *expr, ER ercd)
+{
+	if (ercd < 0) {
+		t_perror(LOG_ERROR, file, line, expr, ercd);
+	}
+}
+
+#define	SVC_PERROR(expr)	svc_perror(__FILE__, __LINE__, #expr, (expr))
+
+
+
+/*
+ *  メインタスク
+ */
+void main_task(intptr_t exinf)
+{
+
+
+	
+	SVC_PERROR(ext_ker());
+	assert(0);
+}
+
